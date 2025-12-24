a68b8ee73433289e3b52730fb26527a0cdd7dac8
Aman Dangaura Tharu <enjoyinglearning69@gmail.com>
2025-12-25 03:41:18 +0545

    .

---
+++ bhaiisab/main.py
@@ -0,0 +1,6 @@
+def main():
+    print("Hello from bhaiisab!")
+
+
+if __name__ == "__main__":
+    main()
---
+++ projects/dangaura/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class DangauraConfig(AppConfig):
+    name = 'dangaura'
